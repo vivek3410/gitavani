4d9706fce4123e1b941fcc4e5d5bb555f5bf4161
vivek3410 <87080539+vivek3410@users.noreply.github.com>
2023-12-27 14:12:13 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu } from 'lucide-react'
+import { useMediaQuery } from 'react-responsive';
 
 export default function Header() {
     const [showMenu, setShowMenu] = React.useState(false);
+    const MenuToggle = useMediaQuery({ query: '(max-width: 640px)' })
     const routes = [
         {
             path: "/",
@@ -27,9 +28,23 @@ export default function Header() {
         //     label: "Portfolio",
         // }
     ]
-    function MenuClick(){
+    function MenuClick() {
         setShowMenu(!showMenu);
     }
+
+    useEffect(() => {
+        const handleResize = () => {
+          // Set showMenu to false when the screen size is greater than or equal to 600px (adjust as needed)
+          if(showMenu && window.innerWidth > 640){
+            setShowMenu(false);
+          }
+        };
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
     return (
         <div className='h-[4em] bg-black w-full flex justify-between items-center px-4'>
             <div className="navbar-brand">
@@ -38,33 +53,33 @@ export default function Header() {
                 </span>
             </div>
             <div >
-                <div className='sm:gap-12 sm:flex mr-4 hidden sm:block'>
-                    {routes.map((route) => (
-                        // <a key={route.path} className='text-white cursor-pointer' href={route.path}>{route.label}</a>
-                        <Link key={route.path} className='hover:no-underline text-white text-none cursor-pointer' to={route.path} >{route.label}</Link>
-                    ))}
-                </div>
-                {/* <FontAwesomeIcon icon="fa-solid fa-bars" width={30} height={30} color='white'/> */}
-                {/* <FontAwesomeIcon icon="fa-solid fa-bars" style={{color: "#fcfcfc",}} className='cursor-pointer'/> */}
-                <div className='cursor-pointer block sm:hidden'>
-                    <Menu className='text-white' onClick={() => MenuClick()} />
-                </div>
-                {showMenu && (
+                {MenuToggle ? (
+                    <div className='sm:gap-12 sm:flex mr-4 hidden sm:block'>
+                        {routes.map((route) => (
+                            // <a key={route.path} className='text-white cursor-pointer' href={route.path}>{route.label}</a>
+                            <Link key={route.path} className='hover:no-underline text-white text-none cursor-pointer' to={route.path} >{route.label}</Link>
+                        ))}
+                    </div>
+                ) : (
                     <>
-                        <div className='absolute shadow-2xl z-30 top-[3.5em] right-5 w-1/4  bg-white rounded-xl'>
-                        {/* <div className=' text-white absolute right-5 cursor-pointer' onClick={() => setShowMenu(false)}>x</div> */}
-                            <div className='flex flex-col gap-4 p-[1em] '>
-                                {routes.map((route) => (
-                                    <>
-                                    <Link key={route.path} className='hover:no-underline text-black text-none cursor-pointer ' to={route.path} >{route.label}</Link>
-                                    {/* <hr className='m-0 p-0'/> */}
-                                    </>
-                                ))}
-                            </div>
+                        <div className='cursor-pointer block sm:hidden'>
+                            <Menu className='text-white' onClick={() => MenuClick()} />
                         </div>
+                        {showMenu ? (
+                            <>
+                                <div className='block sm:hidden  absolute shadow-2xl z-30 top-[3.5em] right-5 w-1/4  bg-white rounded-xl'>
+                                    <div className='flex flex-col gap-4 p-[1em] '>
+                                        {routes.map((route) => (
+                                            <>
+                                                <Link key={route.path} className='hover:no-underline text-black text-none cursor-pointer ' to={route.path} >{route.label}</Link>
+                                            </>
+                                        ))}
+                                    </div>
+                                </div>
+                            </>
+                        ) : null}
                     </>
                 )}
-
             </div>
         </div>
     );
